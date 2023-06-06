4c657a5246e0c6de6dfc0a58ea47dd30dd064f5c
arshit <frontend280@gmail.com>
2023-06-06 13:00:19 +0530

    refactor : update quize api

--- src/controller/quiz.controller.js
+++ src/controller/quiz.controller.js
@@ -3,33 +3,35 @@ const QuizModel = require('../models/quiz.model');
 const { successResponse, errorResponse } = require('../helpers/response');
 
 //============================= Create Quiz ==========================//
-exports.createQuiz = async(req, res)=>{
-    try{
-        let authUser = req.userData;
-    if(authUser.account_type === 'ADMIN'){
-        let body = req.body;
-              await new QuizModel({url: body.url}).save().then((docs)=>{
-                successResponse(201, "quiz created successfully", docs, res);
-              }).catch((err)=>{
-                errorResponse(422, err.message, res);
-              })
-        }else {
-          errorResponse(401, "Unauthurized User", res);
+exports.createQuiz = async (req, res) => {
+  try {
+    let authUser = req.userData;
+    if (authUser.account_type === 'ADMIN') {
+      let body = req.body;
+      await new QuizModel({ url: body.url }).save().then((docs) => {
+        successResponse(201, "quiz created successfully", docs, res);
+      }).catch((err) => {
+        errorResponse(422, err.message, res);
+      })
+    } else {
+      errorResponse(401, "Unauthurized User", res);
     }
-    }catch(err){
-        errorResponse(500, err.messge, res);
-    }
+  } catch (err) {
+    errorResponse(500, err.messge, res);
+  }
 }
 
 //============================= Get Quiz ==========================//
-exports.getQuiz = async (req, res) =>{
-  try{
-    await QuizModel.findOne().sort({_id: -1}).then((docs)=>{
+exports.getQuiz = async (req, res) => {
+  try {
+    await QuizModel.findOne().sort(
+      { created_at: -1 }
+    ).then((docs) => {
       successResponse(200, "quiz retrieved successfully", docs, res);
-    }).catch((err)=>{
+    }).catch((err) => {
       errorResponse(422, err.message, res);
     })
-  }catch(err){
-        errorResponse(500, err.messge, res);
-    }
+  } catch (err) {
+    errorResponse(500, err.messge, res);
+  }
 }
